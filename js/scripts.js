5c4fd3ffbcf6de6cdf11deadb42ea0440844a602
Jackson Lucas <jackychaz84@gmail.com>
2020-07-14 21:30:13 +0300

    add background image and multiplication code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var divide = function(number1,number2){
-    return number1/number2;
+var multiply = function(number1,number2){
+    return number1*number2;
     };
     var number1 = parseInt(prompt("Enter a number:"));
     var number2 = parseInt(prompt("Enter another number:"));
-    alert(divide(number1,number2));
+    alert(multiply(number1,number2));
