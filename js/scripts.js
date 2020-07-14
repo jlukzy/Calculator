556917f9160f76d6e6899e2e38f1deed3f95cb62
Jackson Lucas <jackychaz84@gmail.com>
2020-07-14 21:24:38 +0300

    Add a jumbotron and division code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var subtract = function(number1,number2){
-    return number1-number2;
+var divide = function(number1,number2){
+    return number1/number2;
     };
     var number1 = parseInt(prompt("Enter a number:"));
     var number2 = parseInt(prompt("Enter another number:"));
-    alert(subtract(number1,number2));
+    alert(divide(number1,number2));
